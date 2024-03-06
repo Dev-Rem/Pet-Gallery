ee3f7fa3b93cec77b9973f055967179285308f70
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2024-03-06 12:20:20 +0100

    Create routing using Go router

---
+++ lib/homeScreen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page'),
+    Text('Search Page'),
+    Text('New Post Page'),
+    Text('Chat Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            elevation: 0.5,
+            shadowColor: Color.fromARGB(255, 255, 255, 255),
+            surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
+            title: FittedBox(
+              fit: BoxFit.fitWidth,
+              child: Text(
+                'Pet Gallery',
+                style: TextStyle(
+                    fontFamily: "Sacramento",
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500),
+              ),
+            ),
+          ),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            iconSize: 30.0,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.search),
+                icon: Icon(Icons.search_outlined),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.add_circle),
+                icon: Icon(
+                  Icons.add_circle_outlined,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                  activeIcon: Icon(Icons.chat),
+                  icon: Icon(Icons.chat_outlined),
+                  label: ""),
+              BottomNavigationBarItem(
+                activeIcon: Icon(Icons.pets),
+                icon: Icon(Icons.pets_outlined),
+                label: '',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Color.fromARGB(255, 121, 94, 56),
+            unselectedItemColor: Color.fromARGB(255, 217, 191, 166),
+            onTap: _onItemTapped,
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import "signupScreen.dart";
+import "landingScreen.dart";
+import "homeScreen.dart";
+
+// USE FIREBASE AUTHENTICATION
 
 void main() {
   runApp(MyApp());
 }
 
+final _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const LandingScreen();
+        }),
+    GoRoute(
+        path: '/signupScreen',
+        builder: (BuildContext context, GoRouterState state) {
+          return const SignupScreen();
+        }),
+    GoRoute(
+      path: '/homeScreen',
+      builder: (BuildContext context, GoRouterState state) {
+        return HomeScreen();
+      },
+    ),
+    GoRoute(
+      path: '/signinScreen',
+      builder: (BuildContext context, GoRouterState state) {
+        return HomeScreen();
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       title: 'Pet Gallery',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 121, 94, 56)),
       ),
-      home: MyHomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Chat Page'),
-    Text('New Post Page'),
-    Text('Profile Page'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            elevation: 0.5,
-            shadowColor: Color.fromARGB(255, 255, 255, 255),
-            surfaceTintColor: Color.fromARGB(255, 255, 255, 255),
-            title: FittedBox(
-              fit: BoxFit.fitWidth,
-              child: Text(
-                'Pet Gallery',
-                style: TextStyle(
-                    fontFamily: "Sacramento",
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500),
-              ),
-            ),
-          ),
-          body: Center(
-            child: _widgetOptions.elementAt(_selectedIndex),
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            iconSize: 25.0,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.chat,
-                ),
-                label: '',
-              ),
-              BottomNavigationBarItem(icon: Icon(Icons.add_box), label: ""),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: '',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Color.fromARGB(255, 121, 94, 56),
-            unselectedItemColor: Color.fromARGB(255, 217, 191, 166),
-            onTap: _onItemTapped,
-          ),
-        ));
   }
 }
